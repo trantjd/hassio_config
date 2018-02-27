c8dd96eb3b35de4f14b9db11d8dd0dc4ff85d133
trantjd <github@dillman.us>
2018-02-27 01:23:05 -0500

    gitupdate.sh cleanup

--- gitupdate.sh
+++ gitupdate.sh
@@ -4,7 +4,7 @@ hassio homeassistant check
 
 git add .
 git status
-echo -n "Enter the Description for the Change: " [Minor Update] 
+echo -n "Enter the Description for the Change: "
 read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
